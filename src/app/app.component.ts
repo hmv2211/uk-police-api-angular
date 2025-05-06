a21fc461491299bc07d006e72514800663a0fcca
HMV2211 <114997703+hmv2211@users.noreply.github.com>
2025-05-06 13:13:01 +0100

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./aoo.component.css']
+})
+export class AppComponent {
+  title = 'UK Police API Angular App';
+}
