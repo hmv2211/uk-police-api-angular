3bd60d957787bfc4d8cf0955d43fb3ecde80d3cc
HMV2211 <114997703+hmv2211@users.noreply.github.com>
2025-05-06 13:55:46 +0100

    Update crime-search.component.ts

--- src/app/components/crime-search/crime-search.component.ts
+++ src/app/components/crime-search/crime-search.component.ts
@@ -1,1 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Component({
+  selector: 'app-crime-search',
+  templateURL: './crime-search.component.html',
+  styleUrls: ['./crime-search.component.css']
+})
+export class CrimeSearchComponent {
+  latitude: string = '';
+  longitude: string = '';
+  crimes: any[] = [];
+  error: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  fetchCrimes() {
+    if (!this.latitude || !this.longitude) {
+      this.error = 'Please enter both latitude and longitude.';
+      return;
+    }
+
+    const url = 'https://data.police.uk/api/crimes-street/all-crime?lat=${this.latitude}&lng${this.longitude}`;
+    this.http.get<any[]>(url).subscrive({
+      next: data => {
+        this.crimes = data;
+        this.error = '';
+      },
+      error: err => {
+        this.error = 'Failed to load crime data.';
+        this.crimes = [];
+      }
+    });
+  }
